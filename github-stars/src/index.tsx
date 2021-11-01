921a667e2a89e2f19e8e815ade959120c0dd53d5
Zander Martineau <hi@zander.wtf>
2021-11-01 21:45:17 +0000

    Save stars to localstorage to improve performance

--- github-stars/src/index.tsx
+++ github-stars/src/index.tsx
@@ -1,4 +1,11 @@
-import { getPreferenceValues, List, showToast, ToastStyle } from '@raycast/api'
+import {
+  getLocalStorageItem,
+  getPreferenceValues,
+  List,
+  setLocalStorageItem,
+  showToast,
+  ToastStyle,
+} from '@raycast/api'
 import { useEffect, useState } from 'react'
 import fetch from 'node-fetch'
 import { Response } from './response.model'
@@ -22,6 +29,12 @@ export default function PackageList() {
 
   useEffect(() => {
     const fetchPackages = async (): Promise<void> => {
+      const storedItems = await getLocalStorageItem<string>('github-star-items')
+
+      if (storedItems) {
+        setState({ items: JSON.parse(storedItems) as Response })
+      }
+
       try {
         const response = await fetch(
           `https://api.github.com/users/${githubUsername}/starred?per_page=${resultsCount}`,
@@ -29,6 +42,7 @@ export default function PackageList() {
         const json = await response.json()
         setState({ items: json as Response })
         setLoading(false)
+        await setLocalStorageItem('github-star-items', JSON.stringify(json))
       } catch (error) {
         console.error(error)
         showToast(ToastStyle.Failure, 'Could not fetch stars')
