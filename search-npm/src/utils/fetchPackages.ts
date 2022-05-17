ee0e87842e6fcf0d7febeb9236d5bb54d9be3f66
Zander Martineau <hi@zander.wtf>
2022-05-17 07:13:11 +0100

    update search npm from main raycast repo

--- search-npm/src/utils/fetchPackages.ts
+++ search-npm/src/utils/fetchPackages.ts
@@ -4,14 +4,20 @@ import { NpmsFetchResponse } from '../npmsResponse.model'
 
 export const fetchPackages = async (
   searchTerm = '',
+  signal: AbortSignal,
 ): Promise<NpmsFetchResponse> => {
   try {
     const response = await fetch(
       `https://api.npms.io/v2/search/suggestions?q=${searchTerm}`,
+      { signal },
     )
     const json = await response.json()
     return json as NpmsFetchResponse
-  } catch (error) {
+  } catch (error: any) {
+    if (error.name === 'AbortError') {
+      return []
+    }
+
     console.error(error)
     showToast(Toast.Style.Failure, 'Could not fetch packages')
     return Promise.resolve([])
