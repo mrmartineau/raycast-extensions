e55f576b031304b0b40cc7affbf16f4744d6e6a8
Zander Martineau <mrmartineau@users.noreply.github.com>
2023-08-24 16:53:13 +0100

    Update Otter extension

--- otter/src/search.tsx
+++ otter/src/search.tsx
@@ -1,4 +1,4 @@
-import { List } from '@raycast/api'
+import { Action, ActionPanel, Icon, List } from '@raycast/api'
 import { useState } from 'react'
 import { Item } from './Item'
 import { Bookmark } from './bookmark.model'
@@ -16,6 +16,33 @@ export default function Search() {
       onSearchTextChange={setSearchTerm}
       throttle
     >
+      {searchTerm ? (
+        <List.Item
+          title={`View search results in Otter`}
+          icon={Icon.MagnifyingGlass}
+          actions={
+            <ActionPanel>
+              <Action.OpenInBrowser
+                url={`https://otter.zander.wtf/search?searchTerm=${searchTerm}`}
+                title="Open search in Otter"
+              />
+            </ActionPanel>
+          }
+        />
+      ) : (
+        <List.Item
+          title={`View latest items in Otter`}
+          icon={Icon.Bell}
+          actions={
+            <ActionPanel>
+              <Action.OpenInBrowser
+                url={`https://otter.zander.wtf`}
+                title="Open latest items in Otter"
+              />
+            </ActionPanel>
+          }
+        />
+      )}
       {data?.data.length
         ? data?.data.map((item: Bookmark) => {
             return <Item key={item.id} {...item} />
